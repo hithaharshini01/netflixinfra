3382f34ed39f87bab565c31f6f91f61b0540a2b0
Hitha Harshini.G <hithaharshini01@gmail.com>
2024-10-24 20:44:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-0e0e417dfa2028266"
 instance_type = "t2.medium"
 key_name = "harshii"
-vpc_security_groups_ids = ["sg-0501223a5b9285b26"]
+vpc_security_group_ids = ["sg-0501223a5b9285b26"]
 tags = {
 Name = var.instance_names[count.index]
 }
