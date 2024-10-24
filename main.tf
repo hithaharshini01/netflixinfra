99e177a4b1bfbf1e8e6d2620cc39080cbece91e1
Hitha Harshini.G <hithaharshini01@gmail.com>
2024-10-24 20:49:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-0e0e417dfa2028266"
 instance_type = "t2.medium"
-key_name = "harshii"
+key_name = "harshi"
 vpc_security_group_ids = ["sg-0501223a5b9285b26"]
 tags = {
 Name = var.instance_names[count.index]
